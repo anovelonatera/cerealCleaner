3ef55af24d7564c450f81620e6c004110d9bcec7
unknown <adrian.raul.novelo@citibanamex.com>
2019-09-07 18:17:12 -0500

    Solved!

---
+++ cerealSolver.py
@@ -0,0 +1,18 @@
+#import modules
+import os
+import csv
+
+#open csv
+cerealCSV=os.path.join(".","resources","cereal.csv")
+with open(cerealCSV,"r",encoding="UTF-8") as csvfile:
+     
+    #create csv reader
+    csvreader=csv.reader(csvfile, delimiter=",")
+    csvHeader=next(csvreader)
+    print(f"The header is: {csvHeader}") 
+    #iterate rows (for loop)
+    for row in csvreader:
+    #if statement (if the cereal contains 5+ grams of fiber, print)
+        if float(row[7]) >= 5:
+            #print row if condition meets
+            print(row)
